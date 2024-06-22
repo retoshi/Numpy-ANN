c67f651d5ecf614cd59d71ed7deb72bff5a8758a
Ifeoluwa <ifeolufaro@gmail.com>
2024-06-22 18:32:57 +0100

    refactor

--- blocks.py
+++ blocks.py
@@ -3,23 +3,49 @@ from sklearn.metrics import mean_squared_error as mse
 
 ##### Activation Functions #####
 def sigmoid(x):
-    return 1/1+np.exp(-x)
+    """
+        This activation function returns a value (probability) between 0 and 1
+    """
+    
+    return 1/(1+np.exp(-x))
 
 def tanh(x):
+    """
+        This activation function returns a value between -1 and 1
+    """
+    
     return np.tanh(x)
 
 def relu(x):
+    """
+        This activation function returns a value which is either 0 or 1
+    """
+    
     return np.maximum(x, 0)
 
 def linear(x):
+    """
+        This activation function returns a value which is linear
+    """
+    
     return x
 
-##### Neuron #####
 
+##### Neuron #####
 class Neuron:
-    def __init__(self, weights, bias, activation='sigmoid'):
+    """
+        Neural Network Neuron Implementation
+    """
+    
+    def __init__(self, input_weights, bias, activation):
+        """
+            input_weights: np.array() -> an array of shape n by 1 (n x 1) where n represents the number of inputs or activations from the previous layer
+            bias: np.float64() -> a scalar value of shape 1 by 1
+            activation: str -> the activation function for the neuron
+        """
+        
         self.activation = activation
-        self.weights = weights # np.array of randomly initialized weights
+        self.weights = input_weights # np.array of randomly initialized weights
         self.bias = bias # randomly initialized bias
 
     def __str__(self):
@@ -29,59 +55,84 @@ class Neuron:
         return "Neuron <activation: %s>" % self.activation
 
     def output(self, X):
+        """
+            X: np.array() -> an array of input values of shape m by n (m x n) into the neuron where m represents the number training examples or batch size and             n represents the number of inputs
+        """
+        
+        z = np.dot(X, self.weights) + self.bias
+        
         if self.activation == 'sigmoid':
-            z = np.dot(X, self.weights) + self.bias
-            g = sigmoid(z)
-            return g
+            output = sigmoid(z)
+            return output
+        
         elif self.activation == 'tanh':
-            z = np.dot(X, self.weights) + self.bias
-            g = tanh(z)
-            return g
+            output = tanh(z)
+            return output
+        
         elif self.activation == 'relu':
-            z = np.dot(X, self.weights) + self.bias
-            g = relu(z)
-            return g
+            output = relu(z)
+            return output
+        
         elif self.activation == 'linear':
-            z = np.dot(X, self.weights) + self.bias
-            g = linear(z)
-            return g
+            output = linear(z)
+            return output
 
-##### Dense Layer #####
+##### Layer #####
 
-class Dense:
-     
-     def __init__(self, units, input_shape, activation='sigmoid'):
-        # assuming the input_shape to be a number for now
-        self.units = units
-        self.input_shape = input_shape
-        self.neurons = [Neuron(activation=activation, weights=np.random.rand(input_shape), bias=np.random.rand()) for unit in range(units)] # assigning random weights and biases
-        self.weights = np.array([neuron.weights for neuron in self.neurons]).T
-        self.bias = np.array([neuron.bias for neuron in self.neurons])
-        self.activation = activation
- 
-     def output(self, X):
-        if self.activation == 'sigmoid':
-            z = np.dot(X, self.weights) + self.bias
-            g = sigmoid(z)
-            return g
-        elif self.activation == 'tanh':
-            z = np.dot(X, self.weights) + self.bias
-            g = tanh(z)
-            return g
-        elif self.activation == 'relu':
-            z = np.dot(X, self.weights) + self.bias
-            g = relu(z)
-            return g
-        elif self.activation == 'linear':
-            z = np.dot(X, self.weights) + self.bias
-            g = linear(z)
-            return g
+class Layer:
+    """
+        Neural Network Layer Implementation.
+    """
     
-     def update_weights_and_biases(self, weights, biases):
-        self.weights = weights.T
-        self.biases = biases
+    def __init__(self, num_inputs, num_units, activation='sigmoid'):
+        """
+            num_inputs: int -> number of inputs into the network layer
+            num_units: int -> number of neuron units in a specific layer of a neural network
+            activation: str -> activation for that particular layer
+            self.neurons: np.array -> initialized neurons for the network
+            self.layer_weights: np.array -> l by k (l x k) where l is the number of inputs from the previous layer and k is the number of neurons in the layer
+        """
+        
+        self.num_units = num_units
+        self.activation = activation
+        
+        self.neurons = np.array([
+            Neuron(activation=self.activation, 
+                   input_weights=np.random.randn(num_inputs), 
+                   bias=np.random.randn()) for unit in range(self.num_units)]) # assigning random weights and biases
+        
+        self.layer_weights = np.array([neuron.weights for neuron in self.neurons]).T
+        self.layer_biases = np.array([neuron.bias for neuron in self.neurons])
+
+    def output(self, X):
+        """
+            X: np.array() -> an array of dimension m by l (m x l) where m is the number of training examples and l is the number of inputs from the previous                layer
+            
+            returns outputs -> an array of dimension m by k (m x k) where m is the number of training examples and k is the number of neurons in the layer
+        """
+        
+        z = np.dot(X, self.layer_weights) + self.layer_biases
+        
+        if self.activation == 'sigmoid':
+            output = sigmoid(z)
+            return output
+        
+        elif self.activation == 'tanh':
+            output = tanh(z)
+            return output
+        
+        elif self.activation == 'relu':
+            output = relu(z)
+            return output
+        
+        elif self.activation == 'linear':
+            output = linear(z)
+            return output
+    
+#     def update_weights_and_biases(self, weights, biases):
+#         self.weights = weights.T
+#         self.biases = biases
 
 
 if __name__ == "__main__":
     pass
-    
--- network.py
+++ network.py
@@ -3,156 +3,100 @@ import numpy as np
 import copy
 from sklearn.metrics import mean_squared_error as mse
 
-
 class Network:
     
-    _delta = 0.01
+    _delta = 0.0001
     
-    def __init__(self, input_shape, layers=[]):
-        self.layers = layers # initilaize random weights to the layers
-        self.weights = np.array([np.array([np.array([0.0 for feature in range(layer.input_shape)]) for unit in range(layer.units)]) for layer in self.layers])
-        self.biases = np.array([np.array([0.0 for unit in range(layer.units)]) for layer in self.layers])
+    def __init__(self, layers=[]):
+        self.layers = np.array(layers) # initilaize random weights to the layers
+        self.network_weights = [
+            layer.layer_weights for layer in self.layers
+        ]
+        self.network_biases = [
+            layer.layer_biases for layer in self.layers
+        ]
         
-        #******************************************#
-        self.assign_weights_to_layers()
-        self.init_weights = copy.deepcopy(self.weights)
-        self.init_biases = copy.deepcopy(self.biases)
-        self.init_loss = 0
-        self.loss_metric = ''
-        
-    def re_init(self,):
-        self.weights = np.array([np.array([np.array([0.0 for feature in range(layer.input_shape)]) for unit in range(layer.units)]) for layer in self.layers])
-        self.biases = np.array([np.array([0.0 for unit in range(layer.units)]) for layer in self.layers])
-        
-        #******************************************#
-        self.assign_weights_to_layers()
-        self.init_weights = copy.deepcopy(self.weights)
-        self.init_biases = copy.deepcopy(self.biases)
-        self.init_loss = 0
-        self.loss_metric = ''
-        
-    def assign_weights_to_layers(self,):
-        for layer_idx in range(len(self.weights)):
-            for unit_idx in range(len(self.weights[layer_idx])):
-                self.layers[layer_idx].neurons[unit_idx].weights = self.weights[layer_idx][unit_idx]
-                self.layers[layer_idx].neurons[unit_idx].bias = self.biases[layer_idx][unit_idx]
-        
-    def weights_and_biases(self, ):
-        return self.weights, self.biases
-    
-    def layers(self,):
-        return self.layers
-    
-    def num_weights_and_biases(self, ):
-        num_weights = 0
-        num_biases = 0
-        for layer in self.weights:
-            for unit in layer:
-                for weight in unit:
-                    num_weights += 1
-                    
-        for layer in self.biases:
-            for unit in layer:
-                num_biases += 1
-                
-        return num_weights, num_biases
-    
-    def update_weights_and_biases(self, weights, biases):
-        for layer_idx in range(len(weights)):
-            self.layers[layer_idx].update_weights_and_biases(weights[layer_idx], biases[layer_idx])
+        self.history_loss = np.inf
 
-    def output(self, X):
-        outputs = X
-        for layer in self.layers:
+    def forward(self, X, layers):
+        
+        outputs = X.copy()
+        
+        for layer in layers:
             outputs = layer.output(outputs)
         return outputs
     
-    def loss_function(self, y_true, y_pred):
-        if self.loss_metric == 'mse':
-            return mse(y_true, y_pred)
-        elif self.loss_metric == 'rmse':
-            return mse(y_true, y_pred, squared=False)
-        elif self.loss_metric == 'binary_crossentropy':
-            return -1 * (y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
-        elif self.loss_metric == 'sparse_categorical_crossentropy':
-            pass
-    
-    def fit(self, X, y, loss_metric='mse', learning_rate=0.01, EPOCHS=100):
-        self.re_init()
-        self.loss_metric = loss_metric
-        self.calculate_init_loss(X, y)
+    def update_weights_and_biases(self, X, y, learning_rate=0.01):
+        ref_layers = deepcopy(self.layers)
+        updated_layers = deepcopy(self.layers)
         
-        # forward and backward propagations
-        preds, loss = self.learn_params(X, y, learning_rate, EPOCHS)
-        return preds, loss
-
-    def calculate_init_loss(self, X, y):
-        init_preds = self.output(X)
-        self.init_loss = self.loss_function(init_preds, y)
-    
-    def reinitialize_weights_and_biases(self, weights, biases):
-        self.init_weights = copy.deepcopy(weights)
-        self.init_biases = copy.deepcopy(biases)
+        ref_outputs = self.forward(X, ref_layers)
+        ref_loss = mse(y, ref_outputs) 
         
-    def optimize_learning(self, X, y, loss_metric='mse'):
-        best_eta = 0
-        min_loss = np.inf
-        possible_etas = np.arange(0,1,1e-4)
-        
-        for eta in possible_etas:
-            preds, loss, = self.fit(X, y, loss_metric=loss_metric, learning_rate=eta, EPOCHS=100)
-            if loss < min_loss:
-                best_eta = eta
-                min_loss = loss
-                
-        return self.fit(X, y, loss_metric=loss_metric, learning_rate=best_eta, EPOCHS=100), best_eta
-    
-    def predict(self, test):
-        return self.output(test)
-            
-    def learn_params(self, X, y, learning_rate, EPOCHS):
-        
-        preds_history = []
-        
-        for i in range(EPOCHS):
-            for layer_idx in range(len(self.weights)):
-                for unit_idx in range(len(self.weights[layer_idx])):
-                    for weight_idx in range(len(self.weights[layer_idx][unit_idx])):
-                        weights_copy = copy.deepcopy(self.init_weights)
-                        weights_copy[layer_idx][unit_idx][weight_idx] += 0.01
-
-                        # get prediction from changing the current weight 
-                        self.update_weights_and_biases(weights_copy, self.init_biases)
-                        cur_pred = self.output(X)
-                        
-                        loss = self.loss_function(y, cur_pred)
-                        dJ_dw = (loss - self.init_loss) / 0.01
-                        self.weights[layer_idx][unit_idx][weight_idx] =- (learning_rate * dJ_dw)
-                        self.update_weights_and_biases(self.init_weights, self.init_biases)
-                        
-                    # update the bias term
-                    bias_copy = copy.deepcopy(self.init_biases)
-                    bias_copy[layer_idx][unit_idx] = bias_copy[layer_idx][unit_idx] + 0.01
-                    self.update_weights_and_biases(self.init_weights, bias_copy)
-                    cur_pred = self.output(X)
-                    loss = self.loss_function(y, cur_pred)
-                    dJ_db = (loss - self.init_loss) / 0.01   
+        ## weight updates
+        for layer_idx in range(len(ref_layers)):
+            dynamic_layers = deepcopy(self.layers)
+            for neuron_idx in range(len(dynamic_layers[layer_idx].layer_weights)):
+                for weight_idx in range(len(dynamic_layers[layer_idx].layer_weights[neuron_idx])):
+                    dynamic_layers[layer_idx].layer_weights[neuron_idx][weight_idx] += Network._delta
                     
-                    # update bias parameter
-                    self.biases[layer_idx][unit_idx] -= (learning_rate * dJ_db)
-                    self.update_weights_and_biases(self.init_weights, self.init_biases)
-           
-            # Reassign weights to the layers after each EPOCH
-            self.update_weights_and_biases(self.weights, self.biases)
-            self.reinitialize_weights_and_biases(self.weights, self.biases)
-            new_preds = self.output(X)
-            preds_history.append(new_preds)  
-            new_loss = self.loss_function(y, new_preds)
-            if (new_loss > self.init_loss) and i > 2:
-                print(f"\n{'='*40}End of Training. Convergence Reached{'='*40}\n\n")
-                break 
-            self.init_loss = self.loss_function(y, new_preds) 
-            print(f"Loss after epoch {i+1} ******************{self.init_loss}***********************")
-        if i == 99:
-            print(f"\n{'='*39}End of Training. Specified EPOCHS Exhausted{'='*39}\n\n")
-        return preds_history[-2], self.init_loss
+                    ##### forward #####
+                    outputs = self.forward(X, dynamic_layers)
+#                     print(outputs)
+                    temp_loss = mse(y, outputs)
+                    
+                    #### get change in loss w.r.t change in weight
+                    d_L = temp_loss - ref_loss
+                    d_W = Network._delta
+                    
+                    dL_dW = d_L/d_W
+                    
+                    #### update weights
+                    updated_layers[layer_idx].layer_weights[neuron_idx][weight_idx] -= learning_rate*dL_dW
+                    
+        ## bias updates
+        for layer_idx in range(len(ref_layers)):
+            dynamic_layers = deepcopy(self.layers)
+            for neuron_idx in range(len(dynamic_layers[layer_idx].layer_biases)):
+                dynamic_layers[layer_idx].layer_biases[neuron_idx] += Network._delta
+
+                ##### forward #####
+                outputs = self.forward(X, dynamic_layers)
+                temp_loss = mse(y, outputs)
+#                 print(f"Loss after changing weight by delta: {temp_loss}")
+#                 print(f"Loss after before changing weight by delta: {ref_loss}")
+
+                #### get change in loss w.r.t change in bias
+                d_L = temp_loss - ref_loss
+                d_B = Network._delta
+
+                dL_dB = d_L/d_B
+
+                #### update weights
+                updated_layers[layer_idx].layer_biases[neuron_idx] -= learning_rate*dL_dB
+        new_loss = mse(y, self.forward(X, updated_layers))
+        self.layers = deepcopy(updated_layers)
+        return new_loss
+    
+    def fit(self, X, y, batch_size, epochs=400, learning_rate=0.01):
+        init_lr = learning_rate
+        losses = []
+        num_batches = np.int32(np.ceil(np.float32(len(X)) / batch_size))
+        print(num_batches)
+        for epoch in tqdm(iterable=range(epochs), desc=f'Training for {epochs} epochs', total=len(range(epochs))): # tqdm_notebook
+            epoch_loss = 0
+            for batch_num in range(num_batches):
+                if batch_num == (num_batches - 1):
+                    batch_X = X[batch_num*batch_size:,:]
+                    batch_y = y[batch_num*batch_size:,:]
+                else:
+                    batch_X = X[batch_num*batch_size:(batch_num+1)*batch_size,:]
+                    batch_y = y[batch_num*batch_size:(batch_num+1)*batch_size,:]
+                new_loss = self.update_weights_and_biases(batch_X, batch_y, learning_rate=init_lr)
+                epoch_loss += new_loss
+            losses.append(epoch_loss)
+        return losses
+        
+    def predict(self, X):
+        outputs = self.forward(X, layers=self.layers)
+        return outputs
